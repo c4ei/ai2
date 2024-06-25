806299da8f779366b16b93b95ea83937fe11842e
c4ei <c4ei.net@gmail.com>
2024-06-25 13:19:54 +0900

    1 port use 3333

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,6 @@
-
-
 'use-strict';
 
+// 환경 변수 파일(.env) 로드
 (require('dotenv')).config({ path: './.env' });
 const Express = require('express');
 const Helmet = require('helmet');
@@ -12,7 +11,9 @@ const Cors = require('cors');
 const SocketIO = require('socket.io');
 const BootHelper = require('./Utilities/BootHelper');
 const ChildProcess = require('child_process');
+const Path = require('path');
 
+// 전역 예외 처리 핸들러 설정
 process.on('uncaughtException', (UncaughtServerError) => {
     console.error(UncaughtServerError);
     process.exit(1);
@@ -20,11 +21,12 @@ process.on('uncaughtException', (UncaughtServerError) => {
 
 const GlobalErrorHandler = require('./Controllers/Error');
 const Application = Express();
-const Port = process.env.SERVER_PORT || 3334;
+const Port = process.env.SERVER_PORT || 3333;
 const Hostname = process.env.SERVER_HOST || '0.0.0.0';
 const { HandleStreamedResponse } = require('./Controllers/Chat');
 const { VersionChecker } = require('./Tools/GPT');
 
+// 어플리케이션에 표준화된 바인딩 적용
 BootHelper.StandarizedBindingToApplication({
     Application,
     Suffix: '/api/v1/',
@@ -46,6 +48,19 @@ BootHelper.StandarizedBindingToApplication({
     }
 });
 
+// 정적 파일 제공을 위한 설정 추가
+Application.use(Express.static(Path.join(__dirname, '../client/dist')));
+
+// 루트 경로에 대한 요청 처리
+Application.get('/', function(req, resp) {
+    resp.sendFile(Path.join(__dirname, '../client/dist/index.html'));
+});
+
+// 모든 경로에 대한 요청 처리
+Application.get('*', function(req, resp) {
+    resp.sendFile(Path.join(__dirname, '../client/dist/index.html'));
+});
+
 // 모든 경로('*')에 대한 요청을 처리하는 미들웨어 함수
 Application.all('*', (Request, Response) => {
     // 요청 경로가 '/api/v1/'로 시작하는지 확인
@@ -57,7 +72,7 @@ Application.all('*', (Request, Response) => {
                 Message: 'INVALID_API_REQUEST',
                 URL: Request.originalUrl
             }
-        })
+        });
     }
     // API 요청이 아닌 경우 클라이언트 호스트로 리다이렉트
     Response.redirect(process.env.CLIENT_HOST);
@@ -66,56 +81,32 @@ Application.all('*', (Request, Response) => {
 // 전역 에러 핸들러 적용
 Application.use(GlobalErrorHandler);
 
-// Get the configured HTTP server instance
+// HTTP 서버 인스턴스 가져오기
 const WebServer = BootHelper.GetConfiguredHTTPServerInstance(Application);
 
-// Configure Socket.IO and handle streamed responses
+// Socket.IO 구성 및 스트리밍 응답 처리
 HandleStreamedResponse(SocketIO(WebServer, { cors: { origin: process.env.CORS_ORIGIN } }));
 
-// Start the server
+// 서버 시작
 WebServer.listen(Port, () => {
-    // Periodically check and update the package version
+    // 주기적으로 패키지 버전을 확인하고 업데이트
     setInterval(async () => {
-        try{
+        try {
             const Response = await VersionChecker();
-            if(Response === 'UP_TO_DATE')
-                return; // If the version is up-to-date, do nothing
-            // Update the g4f package
+            if (Response === 'UP_TO_DATE')
+                return; // 버전이 최신인 경우 아무 작업도 수행하지 않음
+            // g4f 패키지 업데이트
             ChildProcess.spawn('pip', ['install', '-U', 'g4f']);
-        }catch(VersionUpdateError){
+        } catch (VersionUpdateError) {
             console.warn(VersionUpdateError);
         }
-    }, 1000 * 60 * process.env.TIME_FOR_CHECK_AND_UPDATE_PACKAGES); // Run every X minutes, as set in the environment variable
+    }, 1000 * 60 * process.env.TIME_FOR_CHECK_AND_UPDATE_PACKAGES); // 환경 변수에 설정된 시간 간격으로 실행
 
-    // Log a success message when the server starts
+    // 서버 시작 시 성공 메시지 로그
     console.log(`Server started successfully on port ${Port}.`);
 });
 
-// // 설정된 HTTP 서버 인스턴스 가져오기
-// const WebServer = BootHelper.GetConfiguredHTTPServerInstance(Application);
-
-// // Socket.IO 설정 및 스트림 응답 처리
-// HandleStreamedResponse(SocketIO(WebServer, { cors: { origin: process.env.CORS_ORIGIN } }));
-
-// // 서버 시작
-// WebServer.listen(Port, Hostname, () => {
-//     // 주기적으로 패키지 버전 체크 및 업데이트
-//     setInterval(async () => {
-//         try{
-//             const Response = await VersionChecker();
-//             if(Response === 'UP_TO_DATE')
-//                 return; // 최신 버전이면 아무것도 하지 않음
-//             // g4f 패키지 업데이트
-//             ChildProcess.spawn('pip', ['install', '-U', 'g4f']);
-//         }catch(VersionUpdateError){
-//             console.warn(VersionUpdateError);
-//         }
-//     }, 1000 * 60 * process.env.TIME_FOR_CHECK_AND_UPDATE_PACKAGES); // 환경 변수에 설정된 시간(분) 간격으로 실행
-
-//     // 서버 시작 성공 메시지 출력
-//     console.log(`서버가 네트워크 주소 (${Hostname}:${Port})에서 성공적으로 시작되었습니다.`);
-// });
-
+// 전역 Promise 거부 처리 핸들러 설정
 process.on('unhandledRejection', (UnhandledServerError) => {
     console.warn(UnhandledServerError);
     WebServer.close(() => process.exit(1));
